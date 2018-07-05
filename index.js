391f4b0a9161d82124b7f1f9b66f366d64836503
Jean-Philippe Roy <jeanphilippe.roy@bell.ca>
2018-07-05 15:15:40 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  rules: {
+    'max-len': ['error', 100, { 'ignoreComments': true }],
+    'object-curly-spacing': ['error', 'always'],
+    'quote-props': ['error', 'consistent'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
+  }
+
+}
