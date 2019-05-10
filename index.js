7ba54ad5146db20120a9f679698df7bec37dd599
Jean-Philippe Roy <jeanphilippe.roy@bell.ca>
2019-05-10 11:24:48 -0400

    Add comma-spacing rule

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = {
 
   rules: {
+    'comma-spacing': ["error", { "before": false, "after": true }],
     'max-len': ['error', 100, { 'ignoreComments': true }],
     'object-curly-spacing': ['error', 'always'],
     'quote-props': ['error', 'consistent'],
     'quotes': ['error', 'single'],
-    'semi': ['error', 'always']
+    'semi': ['error', 'always'],
   }
 
 }
